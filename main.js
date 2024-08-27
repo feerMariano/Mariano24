b56d7f502c49b7fd495cd3b7cd0f6ff1eb7ff924
feerMariano <131987687+feerMariano@users.noreply.github.com>
2024-08-27 07:48:35 -0300

    fer mariano

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+const caixaPrincipal = document.querySelector(".caixaPrincipal");
+const caixaPerguntas = document.querySelector(".caixaPerguntas");
+const caixaAlternativas = document.querySelector(".caixaAlternativas");
+const caixaResultado = document.querySelector(".caixaResultado");
+const caixaTextoResultado = document.querySelector(".caixaTextoResultado");
