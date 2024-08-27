852f441a9e7db7fa41795c5cab6e12c6a779b330
feerMariano <131987687+feerMariano@users.noreply.github.com>
2024-08-27 10:54:30 -0300

    fffffffffffffffffffffffffff

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-const caixaPrincipal = document.querySelector(".caixaPrincipal");
-const caixaPerguntas = document.querySelector(".caixaPerguntas");
-const caixaAlternativas = document.querySelector(".caixaAlternativas");
-const caixaResultado = document.querySelector(".caixaResultado");
-const caixaTextoResultado = document.querySelector(".caixaTextoResultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+        {
+            enunciado: "Pergunta 3",
+            alternativas: ["aternativa1","alternativa2","alternativa3"]
+        }
+        {
+            enunciado: "Pergunta 4",
+            alternativas: ["aternativa1","alternativa2","alternativa3"]
+        }
+     ]
